87e58c5534793d84b95339f8d1f7a88eaefdcc73
Leon <l.lusic@student.vu.nl>
2021-04-28 13:53:07 +0200

    A7 start

---
+++ Assignment 7/Assignment7.R
@@ -0,0 +1,54 @@
+### Exercise 7.1 ###
+
+source("functions_Ch8.txt")
+
+geese = read.csv("geese.txt", sep="\t")
+
+# a)
+
+# b)
+
+# c)
+
+# d)
+
+# e)
+
+# f)
+
+# g)
+
+### Exercise 7.2 ###
+
+pollution = read.csv("airpollution.txt", sep=" ")
+
+# a)
+
+# b)
+
+# c)
+
+# d)
+
+# e)
+
+# f)
+
+# g)
+
+# h)
+
+# i)
+
+# j)
+
+### Exercise 7.3 ###
+
+expenses = read.csv("expensescrime.txt", sep=" ")
+
+# a)
+
+# b)
+
+# c)
+
