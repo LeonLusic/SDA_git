fa508681f2fd56b086bd767e448f97141eeeb7a3
Leon <l.lusic@student.vu.nl>
2021-05-16 20:48:34 +0200

    Update Assignment 7

--- Assignment 7/Assignment7.R
+++ Assignment 7/Assignment7.R
@@ -3,14 +3,38 @@
 source("functions_Ch8.txt")
 
 geese = read.csv("geese.txt", sep="\t")
+attach(geese)
 
 # a)
 
+plot(observer1,photo)
+plot(observer2,photo)
+
 # b)
+
+observer1lm = lm(observer1~photo, data=geese)
+observer2lm = lm(observer2~photo, data=geese)
+
+summary(lm(observer1~photo, data=geese))
+summary(lm(observer2~photo-1, data=geese))
+
+summary(observer1lm)
+
+help(lm)
 
 # c)
 
+plot(observer1lm$residuals, photo)
+plot(observer2lm$residuals, photo)
+
 # d)
+
+lm.norm.test(observer1,photo)
+
+help(lm.norm)
+
+qqnorm(observer1lm$residuals)
+qqnorm(observer2lm$residuals)
 
 # e)
 
@@ -21,12 +45,40 @@ geese = read.csv("geese.txt", sep="\t")
 ### Exercise 7.2 ###
 
 pollution = read.csv("airpollution.txt", sep=" ")
+attach(pollution)
+
+alpha = 0.05
 
 # a)
 
+pairs(cbind(wind, temperature, humidity, insolation))
+
 # b)
 
+windlm = lm(oxidant~wind)
+temperaturelm = lm(oxidant~temperature)
+humiditylm = lm(oxidant~humidity)
+insolationlm = lm(oxidant~insolation)
+
+summary(windlm)
+summary(temperaturelm)
+summary(humiditylm)
+summary(insolationlm)
+
+summary(lm(oxidant~wind+temperature))
+summary(lm(oxidant~wind+humidity))
+summary(lm(oxidant~wind+insolation))
+
+summary(lm(oxidant~wind+temperature+humidity))
+summary(lm(oxidant~wind+temperature+insolation))
+
+stepup_lm = lm(oxidant~wind+temperature)
+
 # c)
+
+lmfull = lm(oxidant~wind+temperature+humidity+insolation)
+
+summary(lmfull)
 
 # d)
 
